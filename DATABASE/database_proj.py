4df2c4a839162a41a6c6f8a22fcf296537de891d
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-04-08 11:34:17 +0600

    select and delet

--- DATABASE/database_proj.py
+++ DATABASE/database_proj.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("db_connection.db")
 
 cursor = conn.cursor()
-
+# CRUD -- > CREATE, RETRIEVE(SELECT), UPDATE, DELETE
 
 class DbTable:
     table_name = None
@@ -35,7 +35,6 @@ class DbTable:
 
         return cols           
 
-
     def insert_data(self, *args):
         print(tuple(args))
         cols = str(tuple(self.column_list))
@@ -52,10 +51,53 @@ class DbTable:
             INSERT INTO {self.table_name} {cols} VALUES ({vals})
         """, tuple(args))
 
+    def select_data(self, **kwargs):
+        if len(kwargs) == 0:
+            cursor.execute(f"SELECT * FROM {self.table_name}")
+
+        else:
+            query_string = self.query_from_kwargs(kwargs)
+            print(query_string)
+            cursor.execute(f"SELECT * FROM {self.table_name} WHERE {query_string}")
+
+        data = cursor.fetchall()
+        print(data)
+
+    def query_from_kwargs(self, kwargs):
+        try:
+            q = kwargs.pop("query_condition")
+        except KeyError:
+            q = "AND"
+
+        vals = list(kwargs.values())
+        keys = list(kwargs.keys())
+
+        query_string = ""
+        for i in range(len(vals)):
+            each_key = keys[i]
+            each_val = vals[i]
+
+            if query_string == "":
+                query_string += f"{each_key}='{each_val}'"
+            else:
+                query_string += f" {q} {each_key}='{each_val}'"
+
+        return query_string
+
+    def delete_data(self, **kwargs):
+        if len(kwargs) == 0:
+            cursor.execute(f"DELETE FROM {self.table_name}")
+        else:
+            query_string = self.query_from_kwargs(kwargs)
+            cursor.execute(f"DELETE FROM {self.table_name} WHERE {query_string}")
+
+
 db = DbTable("Students", "name:TEXT", "age:INTEGER", "contact:TEXT", "DateOfBirth:TEXT")
 db.create_table()
 db.add_columns()
 db.insert_data("John", 50, 123, "15.09.2000")
+db.select_data()
+# db.delete_data()
 
 conn.commit()
 conn.close()
